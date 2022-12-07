0f0d32a18f183a00ab2243f665db67c4563c0036
AditiRout <aditi120000@gmail.com>
2022-12-07 20:50:33 +0530

    new change

--- pages/index.js
+++ pages/index.js
@@ -35,13 +35,13 @@ export default function Home() {
           <link rel='icon' href='/favicon.ico' />
         </Head>
         {/* Overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1] bg-[url(https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2575&q=80)] bg-no-repeat bg-cover bg-center' />
         {/* Background image */}
-        <Image
+        {/* <Image
           src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2575&q=80'
           layout='fill'
           className='object-cover'
-        />
+        /> */}
 
         {/* Search */}
         <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
